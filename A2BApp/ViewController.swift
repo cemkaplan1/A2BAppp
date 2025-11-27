910dbafb57937e5c1b4c588ceed6f400d9e66b5a
cemkaplan1 <cemkaplan74@yahoo.com>
2025-11-27 11:12:35 -0500

    Initial Commit

---
+++ A2BApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  A2BApp
+//
+//  Created by Cem Kaplan on 11/27/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ A2BAppTests/A2BAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  A2BAppTests.swift
+//  A2BAppTests
+//
+//  Created by Cem Kaplan on 11/27/25.
+//
+
+import Testing
+@testable import A2BApp
+
+struct A2BAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
